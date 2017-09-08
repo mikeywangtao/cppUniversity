99233ea5973e30e1285c353012449d5e15da1255
wangtao <niitwangtao@163.com>
2017-09-08 16:15:06 +0800

    No.3 create

---
+++ course003/code/code/code.cpp
@@ -0,0 +1,18 @@
+// code.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "stdlib.h"
+#include "cstdio"
+#include "iostream"
+
+using namespace std;
+
+int main() {
+	cout << "第三章......" << endl;
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
+
