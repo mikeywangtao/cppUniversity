4df7d150fecac31d3a9d426eb5f33ed7d42e0a5b
wangtao <niitwangtao@163.com>
2017-10-15 10:42:58 +0800

    create code 12

---
+++ coutse012/code12/code12/code12.cpp
@@ -0,0 +1,25 @@
+// code12.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "iomanip"
+#include "cstring"
+#include "string"
+#include "cctype"
+#include "cstdlib"
+#include "vector"
+
+using namespace std;
+
+
+int main() {
+	cout << "This is code 12 !!" << endl;
+	cout << endl;
+
+	cout << endl;
+	cout << endl;
+	system("pause");
+  return 0;
+}
+
