52612b2e3b0de4a2e3332517ffc7e33fc5e1d248
wangtao <niitwangtao@163.com>
2017-11-14 08:50:45 +0800

    create 13

---
+++ coutse013/code13/code13/code13/code13.cpp
@@ -0,0 +1,25 @@
+// code13.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "iomanip"
+#include "cstdlib"
+#include "cctype"
+#include "string"
+#include "vector"
+
+using namespace std;
+
+
+int main()
+{
+	cout << "This is code 13!!" << endl;
+	cout << endl;
+
+	cout << endl;
+	cout << endl;
+	system("pause");
+  return 0;
+}
+
