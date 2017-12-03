4c20030690d30cac31c1ae73bebcde5a4109bdcd
wangtao <niitwangtao@163.com>
2017-12-03 10:47:41 +0800

    add code 14

---
+++ coutse014/code14/code14/code14.cpp
@@ -0,0 +1,26 @@
+// code14.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "iomanip"
+#include "cstdlib"
+#include "cctype"
+#include "string"
+#include "vector"
+
+using namespace std;
+
+
+int main(){
+	cout << "This is code 14!!" << endl;
+	cout << endl;
+	cout << endl;
+
+
+	cout << endl;
+	cout << endl;
+	system("pause");
+    return 0;
+}
+
