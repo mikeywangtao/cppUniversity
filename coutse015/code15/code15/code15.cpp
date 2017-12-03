1b843903a3cd1d1cbe27e614ce55b7c0cd180a99
wangtao <niitwangtao@163.com>
2017-12-03 13:41:36 +0800

    create code 15

---
+++ coutse015/code15/code15/code15.cpp
@@ -0,0 +1,27 @@
+// code15.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "iomanip"
+#include "cstdlib"
+#include "cctype"
+#include "string"
+#include "vector"
+
+using namespace std;
+
+
+int main(){
+	cout << "This is code 15!!" << endl;
+	cout << endl;
+	cout << endl;
+
+
+	cout << endl;
+	cout << endl;
+	system("pause");
+
+    return 0;
+}
+
