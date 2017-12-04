70a96d33bf03414af55a0ceae9571310746cfebd
wangtao <niitwangtao@163.com>
2017-12-04 14:08:12 +0800

    create code 16

---
+++ coutse016/code16/code16/code16.cpp
@@ -0,0 +1,25 @@
+// code16.cpp : 定义控制台应用程序的入口点。
+//
+#include "stdafx.h"
+#include "iostream"
+#include "iomanip"
+#include "fstream"
+#include "cstdlib"
+#include "cctype"
+#include "string"
+#include "vector"
+
+using namespace std;
+
+int main(){
+	cout << "This is code 16!!" << endl;
+	cout << endl;
+	cout << endl;
+
+
+	cout << endl;
+	cout << endl;
+	system("pause");
+    return 0;
+}
+
