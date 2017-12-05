19e7012775654e4107dbbd3393c133c8da7d9469
wangtao <niitwangtao@163.com>
2017-12-05 10:20:15 +0800

    create code 17

---
+++ coutse017/code17/code17/code17.cpp
@@ -0,0 +1,30 @@
+// code17.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "iomanip"
+#include "fstream"
+#include "cstdlib"
+#include "cctype"
+#include "memory"
+#include "string"
+#include "vector"
+
+using namespace std;
+
+
+int main() {
+	cout << "This is code 17!! " << endl;
+	cout << endl;
+	cout << endl;
+
+
+
+	cout << endl;
+	cout << endl;
+	system("pause");
+
+	return 0;
+}
+
